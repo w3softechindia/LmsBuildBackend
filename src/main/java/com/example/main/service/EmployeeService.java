1f690132b296d43aa77cc9d81bc279a7f0ac84f2
ChinmayChidwi <126610157+ChinmayChidwi@users.noreply.github.com>
2024-06-06 17:47:55 +0530

    Chinmay (#13)
    * 06-062024
    
    * 06/06/2024

--- src/main/java/com/example/main/service/EmployeeService.java
+++ src/main/java/com/example/main/service/EmployeeService.java
@@ -1,7 +1,13 @@
 package com.example.main.service;
 
+import com.example.main.entity.Employee;
+
 public interface EmployeeService {
 	
-	
+
+	public Employee getEmployeeDetails(String employeeId) throws Exception;
+
+	public Employee updateEmployeeDetails(String employeeId, Employee employee) throws Exception;
+
 
 }
--- src/main/java/com/example/main/serviceImplementation/EmployeeImpl.java
+++ src/main/java/com/example/main/serviceImplementation/EmployeeImpl.java
@@ -1,10 +1,42 @@
 package com.example.main.serviceImplementation;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class EmployeeImpl {
+import com.example.main.entity.Employee;
+import com.example.main.repository.EmployeeRepository;
+import com.example.main.service.EmployeeService;
 
-	
+@Service
+public class EmployeeImpl implements EmployeeService{
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	@Override
+	public Employee getEmployeeDetails(String employeeId) throws Exception {
+
+		Employee employee = employeeRepository.findById(employeeId)
+				.orElseThrow(() -> new Exception(" This Employee ID " + employeeId + " not found"));
+		return employee;
+	}
+
+	@Override
+	public Employee updateEmployeeDetails(String employeeId, Employee employee) throws Exception {
+
+		Employee existingEmployee = employeeRepository.findById(employeeId)
+				.orElseThrow(() -> new Exception("Employee ID " + employeeId + " not found"));
+
+		existingEmployee.setFirstName(employee.getFirstName());
+		existingEmployee.setLastName(employee.getLastName());
+		existingEmployee.setAddress(employee.getAddress());
+		existingEmployee.setWebMail(employee.getWebMail());
+		existingEmployee.setWebMailPassword(employee.getWebMailPassword());
+		existingEmployee.setEmployeeEmail(employee.getEmployeeEmail());
+		existingEmployee.setEmployeePassword(employee.getEmployeePassword());
+		existingEmployee.setPhoneNumber(employee.getPhoneNumber());
+
+		return employeeRepository.save(existingEmployee);
+	}
 
 }
