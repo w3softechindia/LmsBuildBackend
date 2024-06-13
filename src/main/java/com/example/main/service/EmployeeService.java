0d7f8e2c071cafc90a14f9447a2d988c05ab8d7f
Aman Mulla <94896013+Amanmulla6499@users.noreply.github.com>
2024-06-13 09:48:06 +0530

    Update EmployeeService.java

--- src/main/java/com/example/main/service/EmployeeService.java
+++ src/main/java/com/example/main/service/EmployeeService.java
@@ -12,11 +12,6 @@ public interface EmployeeService {
 
 	public Employee updateEmployeeDetails(String employeeId, Employee employee) throws Exception;
 
-
-	public Employee getEmployeeDetails(String employeeId) throws Exception;
-
-	public Employee updateEmployeeDetails(String employeeId, Employee employee) throws Exception;
-
 	public Employee resetPassword(String employeeId, String currentPassword, String newPassword) throws Exception;
 	
 	public Course getCourseByName(String courseName) throws Exception;
