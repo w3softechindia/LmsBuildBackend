1a712c356eb7be3ab52157550f35a5b243d2d297
Amanmulla6499 <amanmulla6499>
2024-07-24 10:07:16 +0530

    24-07-2024

--- src/main/java/com/example/main/entity/Sessions.java
+++ src/main/java/com/example/main/entity/Sessions.java
@@ -1,11 +1,14 @@
 package com.example.main.entity;
 import java.time.LocalDate;
+
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
